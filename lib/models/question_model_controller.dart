a47debb73f31fbf35427c1ad247971304428c166
Samuel Chan <cchin.samuel@gmail.com>
2024-04-17 22:21:23 +0800

    feat: update wordings & algo fix

--- lib/models/question_model_controller.dart
+++ lib/models/question_model_controller.dart
@@ -58,6 +58,13 @@ class QuestionControllerV2 {
     10: const UserAnswer(selectedOptionIndex: []),
   };
 
+  List<DiagnosedBodyType> get currentBodyTypes =>
+      diagnosedIssue.bodyTypes ?? [];
+
+  bool get onlyOneBodyTypeOrDiagnosed =>
+      diagnosedIssue.bodyTypes?.length == 1 ||
+      diagnosedIssue.diagnosedStep != null;
+
   SharedPreferences get prefs => ref.read(sharedPreferencesProvider);
   int get q6MenstruationPainLevel => int.parse(userAnswers[6]!.text!);
   List<PeriodTexture> get textures => diagnosedIssue.periodTexture ?? [];
@@ -69,11 +76,35 @@ class QuestionControllerV2 {
       bodyTypes: [],
     );
     _diagnoseForStep1Signs();
+
     final canDiagnose = _diagnoseForStep2();
     log.info(
       'Diagnosed... ${diagnosedIssue.bodyTypes} ${diagnosedIssue.diagnosedStep}',
     );
     return canDiagnose;
+  }
+
+  List<DiagnosedBodyType> filterBodyTypesByData(
+    List<Map<String, String>> data,
+    bool Function(int, Map<String, String>) filter,
+    List<DiagnosedBodyType> currentBodyTypes,
+  ) {
+    final filteredTypes = data
+        .mapIndexed(
+          (index, map) {
+            if (filter(index, map)) {
+              return index;
+            }
+          },
+        )
+        .whereNotNull()
+        .map((e) => DiagnosedBodyType.values[e])
+        .toList();
+    if (filteredTypes.isEmpty) {
+      log.info('No body types found for filtering, reverting to fallback');
+      return currentBodyTypes;
+    }
+    return filteredTypes;
   }
 
   int getNextUnansweredForDebugging() {
@@ -139,158 +170,82 @@ class QuestionControllerV2 {
     return currentStep + 1;
   }
 
-  bool get onlyOneBodyTypeOrDiagnosed =>
-      diagnosedIssue.bodyTypes?.length == 1 ||
-      diagnosedIssue.diagnosedStep != null;
-
-  /// Can ignore this table just follow the orders:
-  /// 1. Textures,
-  /// 2. have m pain?
-  /// 3. when have pain
-  /// 4. how is the pain
-  /// 5. improve?
-  /// Only for 經痛有 (option b) need both
-  bool _diagnoseForStep2() {
-    if (diagnosedIssue.bodyTypes?.length == 1) {
-      // Done with diagnosis
-      diagnosedIssue = diagnosedIssue.copyWith(diagnosedStep: 0);
-      log.info('Diagnosed at step 1, ${diagnosedIssue.bodyTypes}');
-      return true;
-    }
-
-    final userTextures = _setTexturesData();
-    final periodColor = diagnosedIssue.periodColor ?? [];
-    final currentBodyTypes = diagnosedIssue.bodyTypes ?? [];
-    final currentBodyTypesIndexes =
-        currentBodyTypes.map((e) => e.index).toList();
-
-    log.info('Diagnosing at step 2, $currentBodyTypes');
-
-    void confirmDiagnosisIfApplicable(int step) {
-      if (diagnosedIssue.bodyTypes?.length == 1) {
-        diagnosedIssue = diagnosedIssue.copyWith(diagnosedStep: step);
-      }
-    }
-
-    void performDiagnosisTextureS1() {
-      if (onlyOneBodyTypeOrDiagnosed) return;
-      final bodyTypes = filterBodyTypesByData(
-        texturesData,
-        (index, map) {
-          if (!currentBodyTypesIndexes.contains(index)) {
-            // Must be contained in existing body types
-            return false;
-          }
-          return userTextures.any((e) => map[e.title] == 'T');
-        },
-        currentBodyTypes,
+  void startTest() {
+    const testingIndexes = [2];
+    final testingResults = [];
+    for (var element in testingIndexes) {
+      log.info('Testing for index $element');
+      final data = testingData[element];
+      diagnosedIssue = const DiagnosedIssue();
+      Map<int, UserAnswer> userAnswers = {};
+      data.forEach((key, valueObj) {
+        if (key == 'Questions') return;
+        if (key == '診斷') return;
+        final value = valueObj.toString();
+        if (key == '1') {
+          userAnswers[0] = UserAnswer(
+            dateRange: [
+              DateTime.parse(value.split('-').first),
+              DateTime.parse(value.split('-').last),
+            ],
+          );
+        }
+        // if (key == '2') {
+        //   userAnswers[1] = UserAnswer(text: value);
+        // }
+        if (key == '3') {
+          userAnswers[1] = UserAnswer(selectedOptionIndex: [int.parse(value)]);
+        }
+        if (key == '4') {
+          userAnswers[2] = UserAnswer(text: value);
+        }
+        if (key == '5') {
+          userAnswers[3] = UserAnswer(text: value);
+        }
+        if (key == '6') {
+          userAnswers[4] = UserAnswer(
+            selectedOptionIndex:
+                value.split(',').map((e) => e.toInt()).toList(),
+          );
+        }
+        if (key == '7') {
+          userAnswers[5] = UserAnswer(
+            selectedOptionIndex:
+                value.split(',').map((e) => e.toInt()).toList(),
+          );
+        }
+        if (key == '8') {
+          userAnswers[6] = UserAnswer(text: value);
+        }
+        if (key == '9') {
+          userAnswers[7] = UserAnswer(
+            selectedOptionIndex:
+                value.split(',').map((e) => e.toInt()).toList(),
+          );
+        }
+        if (key == '10') {
+          userAnswers[8] = UserAnswer(
+            selectedOptionIndex:
+                value.split(',').map((e) => e.toInt()).toList(),
+          );
+        }
+        if (key == '11') {
+          userAnswers[9] = UserAnswer(
+            selectedOptionIndex: value.isEmpty
+                ? []
+                : value.split(',').map((e) => e.toInt()).toList(),
+          );
+        }
+      });
+      this.userAnswers = userAnswers;
+      diagnose();
+      testingResults.add(
+        (
+          userAnswers,
+          diagnosedIssue,
+        ),
       );
-      log.info('Diagnosing for textures s1...$bodyTypes');
-      diagnosedIssue = diagnosedIssue.copyWith(bodyTypes: bodyTypes);
-      confirmDiagnosisIfApplicable(1);
     }
-
-    void performDiagnosisWhenPainS2() {
-      if (onlyOneBodyTypeOrDiagnosed) return;
-
-      final hasMPain = q6MenstruationPainLevel > 0;
-      // 有 - BOTH 氣虛+血虛
-      if (diagnosedIssue.periodAmount == PeriodAmountIssue.tooMuch &&
-          diagnosedIssue.periodColor!.contains(PeriodColor.lightRed) &&
-          hasMPain) {
-        diagnosedIssue = diagnosedIssue.copyWith(
-          diagnosedStep: 2,
-          bodyTypes: [
-            DiagnosedBodyType.qiDeficiency,
-            DiagnosedBodyType.bloodDeficiency,
-          ],
-        );
-      } else {
-        const options = ['經前', '經期間', '經後'];
-        final mPainPeriodIndexes = userAnswers[7]?.selectedOptionIndex ?? [];
-        final bodyTypes = filterBodyTypesByData(
-          mPainData,
-          (index, map) {
-            if (!currentBodyTypesIndexes.contains(index)) return false;
-            if (!hasMPain) return map['無'] == 'T';
-            return mPainPeriodIndexes
-                .any((period) => map[options[period]] == 'T');
-          },
-          currentBodyTypes,
-        );
-        diagnosedIssue = diagnosedIssue.copyWith(bodyTypes: bodyTypes);
-      }
-
-      confirmDiagnosisIfApplicable(2);
-      log.info('Diagnosing for when pain s2...${diagnosedIssue.bodyTypes}');
-    }
-
-    void performDiagnosisPainSymptomsS3() {
-      if (onlyOneBodyTypeOrDiagnosed) return;
-
-      final newBodyTypes = diagnoseForStep3PainTypes(
-        userAnswers,
-        questions,
-        diagnosedIssue,
-        filterByCurrentTypes: true,
-      );
-      if (newBodyTypes.$1.isNotEmpty) {
-        diagnosedIssue = diagnosedIssue.copyWith(bodyTypes: newBodyTypes.$1);
-      }
-      confirmDiagnosisIfApplicable(3);
-      log.info('Diagnosing for pain symptoms s3...$newBodyTypes');
-    }
-
-    void performDiagnosisPainChangesS4() {
-      if (onlyOneBodyTypeOrDiagnosed) return;
-      final types = _diagnoseForPainImprovement();
-      if (types.isNotEmpty) {
-        diagnosedIssue = diagnosedIssue.copyWith(bodyTypes: types);
-      }
-      confirmDiagnosisIfApplicable(4);
-      log.info('Diagnosing for pain changes s4...$types');
-    }
-
-    performDiagnosisTextureS1();
-    performDiagnosisWhenPainS2();
-    performDiagnosisPainSymptomsS3();
-    performDiagnosisPainChangesS4();
-    // TODO: Diagnose with other questions
-    return false;
-    if (diagnosedIssue.periodAmount == PeriodAmountIssue.tooLittle) {
-      if (periodColor.contains(PeriodColor.lightRed)) {
-        _diagnoseForLightRedLittleBlood();
-      }
-      if (periodColor.contains(PeriodColor.brightRed)) {
-        _diagnoseForBrightRedLittleBlood();
-      }
-
-      if (periodColor.contains(PeriodColor.purpleRed)) {
-        _diagnoseForPurpleRedLittleBlood();
-      }
-    } else if (diagnosedIssue.periodAmount == PeriodAmountIssue.tooMuch) {
-      if (periodColor.contains(PeriodColor.lightRed)) {
-        _diagnoseForLightRedMuchBlood();
-      }
-      if (periodColor.contains(PeriodColor.brightRed)) {
-        _diagnoseForBrightRedMuchBlood();
-      }
-      if (periodColor.contains(PeriodColor.purpleRed) &&
-          userTextures.contains(PeriodTexture.sticky) &&
-          userTextures.contains(PeriodTexture.withBloodClots)) {
-        // (f) 月經過多+紫紅+粘膩+有血塊 options
-        _diagnoseForPurpleRedLittleBlood();
-      }
-    }
-
-    if (diagnosedIssue.bodyTypes?.length == 1) {
-      diagnosedIssue = diagnosedIssue.copyWith(diagnosedStep: 2);
-      return true;
-    } else if (diagnosedIssue.bodyTypes!.length > 1) {
-      return false;
-    }
-
-    throw Exception('Unexpected step!!');
   }
 
   void _diagnoseFinalResult() {
@@ -417,12 +372,13 @@ class QuestionControllerV2 {
   }
 
   List<DiagnosedBodyType> _diagnoseForPainImprovement() {
-    final painImprovement = userAnswers[10]?.selectedOptionIndex;
+    final painImprovement = userAnswers[9]?.selectedOptionIndex;
     if (painImprovement == null || painImprovement.isEmpty) {
       return [];
     }
     final selectedOptions =
-        painImprovement.map((e) => questions[10].options[e]).toList();
+        painImprovement.map((e) => questions[9].options[e]).toList();
+    log.info('Selected options for pain improvement: $selectedOptions');
     final signs = painImprovementData
         .mapIndexed(
           (index, map) {
@@ -440,29 +396,6 @@ class QuestionControllerV2 {
         .toList();
     log.info('Diagnosing for pain improvement...$signs');
     return signs;
-  }
-
-  List<DiagnosedBodyType> filterBodyTypesByData(
-    List<Map<String, String>> data,
-    bool Function(int, Map<String, String>) filter,
-    List<DiagnosedBodyType> currentBodyTypes,
-  ) {
-    final filteredTypes = data
-        .mapIndexed(
-          (index, map) {
-            if (filter(index, map)) {
-              return index;
-            }
-          },
-        )
-        .whereNotNull()
-        .map((e) => DiagnosedBodyType.values[e])
-        .toList();
-    if (filteredTypes.isEmpty) {
-      log.info('No body types found for filtering, reverting to fallback');
-      return currentBodyTypes;
-    }
-    return filteredTypes;
   }
 
   /// (e) 月經過少+紫紅 options
@@ -523,6 +456,9 @@ class QuestionControllerV2 {
         .whereNot((element) => element == 'TBC' || element.trim().isEmpty)
         .map((e) => DiagnosedBodyType.fromString(e))
         .toList();
+    if (issuesFromMatch.isEmpty) {
+      issuesFromMatch.addAll(DiagnosedBodyType.values);
+    }
     diagnosedIssue = diagnosedIssue.copyWith(
       bodyTypes: issuesFromMatch,
     );
@@ -530,6 +466,157 @@ class QuestionControllerV2 {
       'Diagnosing for step 2...$periodAmountIssue, $colors, cannotDiagnose: $cannotDiagnose $issuesFromMatch',
     );
     return issuesFromMatch.length == 1 && !cannotDiagnose;
+  }
+
+  /// Can ignore this table just follow the orders:
+  /// 1. Textures,
+  /// 2. have m pain?
+  /// 3. when have pain
+  /// 4. how is the pain
+  /// 5. improve?
+  /// Only for 經痛有 (option b) need both
+  bool _diagnoseForStep2() {
+    if (diagnosedIssue.bodyTypes?.length == 1) {
+      // Done with diagnosis
+      diagnosedIssue = diagnosedIssue.copyWith(diagnosedStep: 0);
+      log.info('Diagnosed at step 1, ${diagnosedIssue.bodyTypes}');
+      return true;
+    }
+
+    final userTextures = _setTexturesData();
+    final periodColor = diagnosedIssue.periodColor ?? [];
+
+    final currentBodyTypesIndexes =
+        currentBodyTypes.map((e) => e.index).toList();
+
+    log.info('Diagnosing at step 2, $currentBodyTypes');
+
+    void confirmDiagnosisIfApplicable(int step) {
+      if (diagnosedIssue.bodyTypes?.length == 1) {
+        diagnosedIssue = diagnosedIssue.copyWith(diagnosedStep: step);
+      }
+    }
+
+    void performDiagnosisTextureS1() {
+      if (onlyOneBodyTypeOrDiagnosed) return;
+      final bodyTypes = filterBodyTypesByData(
+        texturesData,
+        (index, map) {
+          if (!currentBodyTypesIndexes.contains(index)) {
+            // Must be contained in existing body types
+            return false;
+          }
+          return userTextures.any((e) => map[e.title] == 'T');
+        },
+        currentBodyTypes,
+      );
+      log.info('Diagnosing for textures s1...$bodyTypes');
+      diagnosedIssue = diagnosedIssue.copyWith(bodyTypes: bodyTypes);
+      confirmDiagnosisIfApplicable(1);
+    }
+
+    void performDiagnosisWhenPainS2() {
+      if (onlyOneBodyTypeOrDiagnosed) return;
+
+      final hasMPain = q6MenstruationPainLevel > 0;
+      // 有 - BOTH 氣虛+血虛
+      if (diagnosedIssue.periodAmount == PeriodAmountIssue.tooMuch &&
+          diagnosedIssue.periodColor!.contains(PeriodColor.lightRed) &&
+          hasMPain) {
+        diagnosedIssue = diagnosedIssue.copyWith(
+          diagnosedStep: 2,
+          bodyTypes: [
+            DiagnosedBodyType.qiDeficiency,
+            DiagnosedBodyType.bloodDeficiency,
+          ],
+        );
+      } else {
+        const options = ['經前', '經期間', '經後'];
+        final mPainPeriodIndexes = userAnswers[7]?.selectedOptionIndex ?? [];
+        final bodyTypes = filterBodyTypesByData(
+          mPainData,
+          (index, map) {
+            if (!currentBodyTypesIndexes.contains(index)) return false;
+            if (!hasMPain) return map['無'] == 'T';
+            return mPainPeriodIndexes
+                .any((period) => map[options[period]] == 'T');
+          },
+          currentBodyTypes,
+        );
+        log.info(bodyTypes);
+        diagnosedIssue = diagnosedIssue.copyWith(bodyTypes: bodyTypes);
+      }
+
+      confirmDiagnosisIfApplicable(2);
+      log.info('Diagnosing for when pain s2...${diagnosedIssue.bodyTypes}');
+    }
+
+    void performDiagnosisPainSymptomsS3() {
+      if (onlyOneBodyTypeOrDiagnosed) return;
+
+      final newBodyTypes = diagnoseForStep3PainTypes(
+        userAnswers,
+        questions,
+        diagnosedIssue,
+        filterByCurrentTypes: true,
+      );
+      if (newBodyTypes.$1.isNotEmpty) {
+        diagnosedIssue = diagnosedIssue.copyWith(bodyTypes: newBodyTypes.$1);
+      }
+      confirmDiagnosisIfApplicable(3);
+      log.info('Diagnosing for pain symptoms s3...$newBodyTypes');
+    }
+
+    void performDiagnosisPainChangesS4() {
+      if (onlyOneBodyTypeOrDiagnosed) return;
+      final types = _diagnoseForPainImprovement();
+      if (types.isNotEmpty) {
+        diagnosedIssue = diagnosedIssue.copyWith(bodyTypes: types);
+      }
+      confirmDiagnosisIfApplicable(4);
+      log.info('Diagnosing for pain changes s4...$types');
+    }
+
+    performDiagnosisTextureS1();
+    performDiagnosisWhenPainS2();
+    performDiagnosisPainSymptomsS3();
+    performDiagnosisPainChangesS4();
+    // TODO: Diagnose with other questions
+    return false;
+    if (diagnosedIssue.periodAmount == PeriodAmountIssue.tooLittle) {
+      if (periodColor.contains(PeriodColor.lightRed)) {
+        _diagnoseForLightRedLittleBlood();
+      }
+      if (periodColor.contains(PeriodColor.brightRed)) {
+        _diagnoseForBrightRedLittleBlood();
+      }
+
+      if (periodColor.contains(PeriodColor.purpleRed)) {
+        _diagnoseForPurpleRedLittleBlood();
+      }
+    } else if (diagnosedIssue.periodAmount == PeriodAmountIssue.tooMuch) {
+      if (periodColor.contains(PeriodColor.lightRed)) {
+        _diagnoseForLightRedMuchBlood();
+      }
+      if (periodColor.contains(PeriodColor.brightRed)) {
+        _diagnoseForBrightRedMuchBlood();
+      }
+      if (periodColor.contains(PeriodColor.purpleRed) &&
+          userTextures.contains(PeriodTexture.sticky) &&
+          userTextures.contains(PeriodTexture.withBloodClots)) {
+        // (f) 月經過多+紫紅+粘膩+有血塊 options
+        _diagnoseForPurpleRedLittleBlood();
+      }
+    }
+
+    if (diagnosedIssue.bodyTypes?.length == 1) {
+      diagnosedIssue = diagnosedIssue.copyWith(diagnosedStep: 2);
+      return true;
+    } else if (diagnosedIssue.bodyTypes!.length > 1) {
+      return false;
+    }
+
+    throw Exception('Unexpected step!!');
   }
 
   List<PeriodTexture> _setTexturesData() {
@@ -541,70 +628,14 @@ class QuestionControllerV2 {
             )
             .toList() ??
         [];
-    diagnosedIssue = diagnosedIssue.copyWith(periodTexture: textures);
+    // FIXME: Should not happen in future in the questionnaire
+    if (textures.contains(PeriodTexture.withBloodClots)) {
+      textures.remove(PeriodTexture.dilute);
+    }
+    diagnosedIssue = diagnosedIssue.copyWith(
+      periodTexture: textures,
+    );
     return textures;
-  }
-
-  void startTest() {
-    final data = testingData[1];
-    Map<int, UserAnswer> userAnswers = {};
-    data.forEach((key, valueObj) {
-      if (key == 'Questions') return;
-      if (key == '診斷') return;
-      final value = valueObj.toString();
-      if (key == '1') {
-        userAnswers[0] = UserAnswer(
-          dateRange: [
-            DateTime.parse(value.split('-').first),
-            DateTime.parse(value.split('-').last),
-          ],
-        );
-      }
-      // if (key == '2') {
-      //   userAnswers[1] = UserAnswer(text: value);
-      // }
-      if (key == '3') {
-        userAnswers[1] = UserAnswer(selectedOptionIndex: [int.parse(value)]);
-      }
-      if (key == '4') {
-        userAnswers[2] = UserAnswer(text: value);
-      }
-      if (key == '5') {
-        userAnswers[3] = UserAnswer(text: value);
-      }
-      if (key == '6') {
-        userAnswers[4] = UserAnswer(
-          selectedOptionIndex: value.split(',').map((e) => e.toInt()).toList(),
-        );
-      }
-      if (key == '7') {
-        userAnswers[5] = UserAnswer(
-          selectedOptionIndex: value.split(',').map((e) => e.toInt()).toList(),
-        );
-      }
-      if (key == '8') {
-        userAnswers[6] = UserAnswer(text: value);
-      }
-      if (key == '9') {
-        userAnswers[7] = UserAnswer(
-          selectedOptionIndex: value.split(',').map((e) => e.toInt()).toList(),
-        );
-      }
-      if (key == '10') {
-        userAnswers[8] = UserAnswer(
-          selectedOptionIndex: value.split(',').map((e) => e.toInt()).toList(),
-        );
-      }
-      if (key == '11') {
-        userAnswers[9] = UserAnswer(
-          selectedOptionIndex: value.isEmpty
-              ? []
-              : value.split(',').map((e) => e.toInt()).toList(),
-        );
-      }
-    });
-    this.userAnswers = userAnswers;
-    diagnose();
   }
 }
 
@@ -628,6 +659,9 @@ class QuestionControllerV2 {
   final selectedOptionsInText = painTypeIndexes
       .map((e) => questions[8].optionsByLastAnsIndex(lastAnsIndexes)[e])
       .toList();
+  log.info(
+    'Selected options in text: $selectedOptionsInText, $painTypeIndexes',
+  );
   final signsFromPainTypes = selectedOptionsInText
       .map((e) => menstruationPainData[e]!.split('\n'))
       .flatten()
@@ -641,6 +675,6 @@ class QuestionControllerV2 {
             : true,
       )
       .toList();
-  log.info('Diagnosing for step 3...$bodyTypes');
+  // log.info('Diagnosing for step 3 pain types...$bodyTypes');
   return (bodyTypes, bodyTypes.isNotEmpty);
 }
